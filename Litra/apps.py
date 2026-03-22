b34a24b6ca5da0eb1e4d395dd2c983478401e558
thelight21313 <artemijpopkov6@gmail.com>
2026-03-22 15:23:52 +0400

    fix

--- Litra/apps.py
+++ Litra/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class LitraConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Litra'
+
+    def ready(self):
+        import Litra.signals
